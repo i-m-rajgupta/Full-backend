14319952737f5d4ee76774129d0fa9c3179a8c08
Raj <neeleshgupta32154@gmail.com>
2025-10-04 01:25:26 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/api",(req,res)=>{
+    res.send("Hello world");
+})
+
+app.get("/api/jokes",(req,res)=>{
+    const jokes = [{
+      id:1,
+      title:"Good",
+      joke : "Why don't skeletons fight each other? Because they don't have the guts."
+    },
+    {
+        id :2 ,
+        title:"thik",
+        joke : "Why did the scarecrow win an award? Because he was outstanding in his field!"
+    }
+]
+      res.send(jokes);
+});
+
+
+
+// const port = process.env.PORT || 8080;
+
+// app.listen(port,(req,res)=>{
+//     console.log("Server is serving ");
+// })
+
+module.exports = app 
